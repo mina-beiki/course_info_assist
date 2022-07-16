600194d245b869fa4d2bf119a63881233e7c86af
mina-beiki <mina_beiki@yahoo.com>
2022-07-16 13:46:40 +0430

    added getting input from user

---
+++ src/Main.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main() {
+    println("Welcome to your Course Assistant!")
+    print("Enter the name of course: ")
+
+    val scanner = Scanner(System.`in`)
+    val courseName: String = scanner.next()
+    //println("Course name = $courseName")
+
+
+}
