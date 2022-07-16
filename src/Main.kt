509d49fda9f996582151795bd17f58344cc430a4
mina-beiki <mina_beiki@yahoo.com>
2022-07-16 15:19:49 +0430

    added Jsoup library

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,16 @@
+import org.jsoup.Jsoup
 import java.util.*
 
+
 fun main() {
+
     println("Welcome to your Course Assistant!")
     print("Enter the name of course: ")
 
-    val scanner = Scanner(System.`in`)
-    val courseName: String = scanner.next()
+    //val scanner = Scanner(System.`in`)
+    //val courseName: String = scanner.next()
     //println("Course name = $courseName")
 
-
+    val doc = Jsoup.connect("https://ufind.univie.ac.at/en/vvz.html").get()
+    print(doc.body().allElements.toString())
 }
