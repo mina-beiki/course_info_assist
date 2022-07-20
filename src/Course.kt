0390cb50f4a5c19705b6e78c53724f55336395e6
mina-beiki <mina_beiki@yahoo.com>
2022-07-20 14:43:10 +0430

    added the new part

--- src/Course.kt
+++ src/Course.kt
@@ -1,10 +1,29 @@
 class Course(name: String, link: String) {
     var courseName: String = name
     var courseLink: String = link
-    lateinit var events: ArrayList<String>
+    lateinit var event: String
+    lateinit var date: String
 
-    fun setEventsList(eventsList: ArrayList<String>){
-        events = eventsList
+    fun setEventString(eventTime: String) {
+        event = eventTime
+    }
+
+    fun generateDate() {
+        if (event.isNotEmpty()) {
+            if (event.substring(0, 5) == "Moodle") {
+                println("moodle")
+                date = event.substring(10, 15)
+            } else if (event.substring(0, 2) == "Gr.") {
+                println("gr")
+                date = ""
+            } else {
+                println("normal")
+                date = event.substring(3, 8)
+            }
+        } else {
+            println("empty")
+            date = ""
+        }
     }
 
 }
--- src/Main.kt
+++ src/Main.kt
@@ -5,6 +5,8 @@ import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.system.exitProcess
 
+//import kotlinx.coroutines
+
 
 fun main() {
 
@@ -64,9 +66,9 @@ fun main() {
         for (e in siblings) {
             println("$ctr - ${e.text()}")
             //println(e.children()[0].attr("abs:href"))
-            val sProgram = StudyProgram(e.text(),e.children()[0].attr("abs:href"))
+            val sProgram = StudyProgram(e.text(), e.children()[0].attr("abs:href"))
             sProgramList.add(sProgram)
-            ctr ++
+            ctr++
         }
     }
 
@@ -76,6 +78,13 @@ fun main() {
     println("Chosen study program = ${sProgramList.get(sProgramNumber).studyProgramName}")
     println(sProgramList.get(sProgramNumber).studyProgramLink)
 
+    println("Choose the task you want to do: (Enter index number)")
+    println("1. View all events for a course")
+    println("2. View next 10 events")
+
+    val index: Int = scanner.nextInt()
+
+
     println("Loading ...")
     try {
         doc = Jsoup.connect(sProgramList.get(sProgramNumber).studyProgramLink).userAgent("Firefox").get()
@@ -84,100 +93,137 @@ fun main() {
         exitProcess(-1)
     }
 
-    println("Successfully loaded!")
-    println()
+
     //list courses:
 
 
-    var courses: Elements  = doc.getElementsByClass("list course level2")
-    var coursesList : ArrayList<Course> = ArrayList()
+    var courses: Elements = doc.getElementsByClass("list course level2")
+    var coursesList: ArrayList<Course> = ArrayList()
 
-    if(doc.getElementsByClass("list course level4").size != 0){
-        for(e in doc.getElementsByClass("list course level4")){
+    if (doc.getElementsByClass("list course level4").size != 0) {
+        for (e in doc.getElementsByClass("list course level4")) {
             courses.add(e)
         }
     }
-    if(doc.getElementsByClass("list course level3").size != 0){
-        for(e in doc.getElementsByClass("list course level3")){
+    if (doc.getElementsByClass("list course level3").size != 0) {
+        for (e in doc.getElementsByClass("list course level3")) {
             courses.add(e)
         }
     }
-    if(doc.getElementsByClass("list course level2").size != 0){
-        for(e in doc.getElementsByClass("list course level2")){
+    if (doc.getElementsByClass("list course level2").size != 0) {
+        for (e in doc.getElementsByClass("list course level2")) {
             courses.add(e)
         }
     }
-    if(doc.getElementsByClass("list course level1").size != 0){
-        for(e in doc.getElementsByClass("list course level1")){
+    if (doc.getElementsByClass("list course level1").size != 0) {
+        for (e in doc.getElementsByClass("list course level1")) {
             courses.add(e)
         }
     }
 
-    if(courses.size==0){
-        println("There are no courses for this study program!")
-        exitProcess(-1)
-    }
+    if (index == 1) {
+        println("Successfully loaded!")
+        println()
 
-    //println(courses.size)
-    ctr = 0
-    for(e in courses){
-        println("$ctr - ${e.text()}")
-        //println(e.children().select("a.what").attr("abs:href"))
-        val c: Course = Course(e.text(),e.children().select("a.what").attr("abs:href"))
-        coursesList.add(c)
-        ctr ++
-    }
-    sProgramList.get(sProgramNumber).setCoursesList(coursesList)
+        if (courses.size == 0) {
+            println("There are no courses for this study program!")
+            exitProcess(-1)
+        }
 
-    //choose course:
+        //println(courses.size)
+        ctr = 0
+        for (e in courses) {
+            println("$ctr - ${e.text()}")
+            //println(e.children().select("a.what").attr("abs:href"))
+            val c: Course = Course(e.text(), e.children().select("a.what").attr("abs:href"))
+            coursesList.add(c)
+            ctr++
+        }
+        sProgramList.get(sProgramNumber).setCoursesList(coursesList)
 
-    println()
-    println("Choose your course: (Enter index number)")
-    val courseNumber: Int = scanner.nextInt()
-    var course: Course = sProgramList.get(sProgramNumber).courses.get(courseNumber)
-    println("Chosen course's name = ${course.courseLink}")
-    println("Chosen course's link = ${course.courseName}")
-    //print(doc.body())
+        //choose course:
+        println()
+        println("Choose your course: (Enter index number)")
+        val courseNumber: Int = scanner.nextInt()
+        var course: Course = sProgramList.get(sProgramNumber).courses.get(courseNumber)
+        println("Chosen course's name = ${course.courseLink}")
+        println("Chosen course's link = ${course.courseName}")
+        //print(doc.body())
 
-    println()
-    println("Loading events ...")
-    try {
-        doc = Jsoup.connect(course.courseLink).userAgent("Firefox").get()
-    } catch (e: Exception) {
-        println("Network error occurred! Try again.")
-        exitProcess(-1)
-    }
+        println()
+        println("Loading events ...")
+        try {
+            doc = Jsoup.connect(course.courseLink).userAgent("Firefox").get()
+        } catch (e: Exception) {
+            println("Network error occurred! Try again.")
+            exitProcess(-1)
+        }
 
-    println("Successfully loaded!")
-
-
-    //print events:
-    val eventsParent: Elements = doc.select("div.usse-id-group")
-    val children: Elements = eventsParent[0].children()
-    var events: ArrayList<String> = ArrayList()
+        println("Successfully loaded!")
 
 
-    for(e in children){
-        if(e.getElementsByClass("event line future").size>0 || e.getElementsByClass("event line next").size>0){
-            for(e2 in e.getElementsByClass("event line next")){
-                events.add(e2.text())
+        //print events:
+        val eventsParent: Elements = doc.select("div.usse-id-group")
+        val children: Elements = eventsParent[0].children()
+        var events: ArrayList<String> = ArrayList()
+
+
+        for (e in children) {
+            if (e.getElementsByClass("event line future").size > 0 || e.getElementsByClass("event line next").size > 0) {
+                for (e2 in e.getElementsByClass("event line next")) {
+                    events.add(e2.text())
+                }
+                for (e2 in e.getElementsByClass("event line future")) {
+                    events.add(e2.text())
+                }
             }
-            for(e2 in e.getElementsByClass("event line future")) {
-                events.add(e2.text())
-            }
         }
+
+        if (events.size == 0) {
+            println("Currently no class schedule is known!")
+            exitProcess(-1)
+        }
+
+        for (eventStr in events) {
+            println(eventStr)
+        }
+    } else if (index == 2) {
+
+        ctr = 0
+        for (e in courses) {
+            val c: Course = Course(e.text(), e.children().select("a.what").attr("abs:href"))
+            coursesList.add(c)
+            ctr++
+        }
+        sProgramList.get(sProgramNumber).setCoursesList(coursesList)
+
+        var threadsCtr: Int = 0
+        //println(coursesList.size)
+        for (c in coursesList) {
+            Thread(Runnable {
+                try {
+                    doc = Jsoup.connect(c.courseLink).userAgent("Firefox").get()
+                } catch (e: Exception) {
+                    c.setEventString("")
+                }
+                //println(c.courseLink)
+                val children: Elements = doc.select("div.usse-id-courselong")[0].children()
+                for (e in children) {
+                    if (e.getElementsByClass("summary groups").size > 0) {
+                        c.setEventString(e.getElementsByClass("summary groups")[0].text())
+                        if(e.getElementsByClass("summary groups")[0].text().length>0 && e.getElementsByClass("summary groups")[0].text().length<120) {
+                            println(e.getElementsByClass("summary groups")[0].text())
+                        }
+                        //c.generateDate()
+                        break
+                    }
+                }
+                threadsCtr++
+                //print("$threadsCtr ")
+            }).start()
+        }
+
     }
 
-    if(events.size==0){
-        println("Currently no class schedule is known!")
-        exitProcess(-1)
-    }
-
-    for(eventStr in events){
-        println(eventStr)
-    }
-
-
-    //println(doc.body())
 
 }
